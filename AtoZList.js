06a3499ba48fa8b16aeb89e590aade3fa6e31b39
Prakash Saini <prakashw3expert@gmail.com>
2018-09-06 18:59:09 +0530

    Update AtoZList.js

--- AtoZList.js
+++ AtoZList.js
@@ -81,6 +81,7 @@ export default class AtoZList extends Component {
       <View style={{flex: 1}}>
         <View style={styles.container}>
           <FixedHeightWindowedListView
+            {...this.props}
             ref={view => this._listView = view}
             dataSource={this.state.dataSource}
             renderCell={this.props.renderCell}
